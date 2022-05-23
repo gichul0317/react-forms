69c5f614b2706c4fbbbc122d24b7172134e03878
gichul <gichul0317@gmail.com>
2022-05-23 17:25:09 -0400

    input validation state updated

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 
 const SimpleInput = (props) => {
   const [name, setName] = useState('');
+  const [valid, setValid] = useState(true);
 
   const inputHandling = (e) => {
     setName(e.target.value);
@@ -10,15 +11,23 @@ const SimpleInput = (props) => {
   const submitHandling = (e) => {
     e.preventDefault();
 
+    if (name.trim() === '') {
+      setValid(false);
+      return;
+    }
+    setValid(true);
     console.log(name);
     setName('');
   };
 
+  const nameInputClasses = valid ? 'form-control' : 'form-control invalid';
+
   return (
     <form onSubmit={submitHandling}>
-      <div className="form-control">
+      <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input type="text" id="name" onChange={inputHandling} value={name} />
+        {!valid && <p className="error-text">input must not be empty.</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
