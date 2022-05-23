d881e7a460fa5e9ab06b228683f393bedfd804f6
gichul <gichul0317@gmail.com>
2022-05-23 15:46:37 -0400

    input state updated

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,24 @@
+import { useState } from 'react';
+
 const SimpleInput = (props) => {
+  const [name, setName] = useState('');
+
+  const inputHandling = (e) => {
+    setName(e.target.value);
+  };
+
+  const submitHandling = (e) => {
+    e.preventDefault();
+
+    console.log(name);
+    setName('');
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={submitHandling}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input type="text" id="name" onChange={inputHandling} value={name} />
       </div>
       <div className="form-actions">
         <button>Submit</button>
